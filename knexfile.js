499a9dc6e14fca050b619fcb822a30a76cc8a9f4
Colby Baker <Colbyco017@gmail.com>
2021-09-05 19:45:48 -0400

    fix ssl stuff

--- knexfile.js
+++ knexfile.js
@@ -1,7 +1,6 @@
 require('dotenv').config();
 module.exports = {
   client: 'pg',
-  connection: process.env.DATABASE_URL,
-  ssl: { rejectUnauthorized: false },
+  connection: { connectionString: process.env.DATABASE_URL, ssl: { rejectUnauthorized: false } },
   searchPath: ['public'],
 };
